b63afe4c091d126cdaf05b74476849a6b2923855
Yashashvi Mann <yashasvimann1@gmail.com>
2023-07-15 13:31:15 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config('Sales Insight',layout='wide')
+
+st.header('Sales Insight Report')
+
+
+st.markdown('<iframe title="Sales_Insight" width="1350" height="680" src="https://app.powerbi.com/reportEmbed?reportId=b5399676-ad43-4537-af9a-9578d836a119&autoAuth=true&ctid=edc5c3bf-4ab5-4697-84fa-41b44eb08b5e" frameborder="0" allowFullScreen="true"></iframe>',unsafe_allow_html=True)
